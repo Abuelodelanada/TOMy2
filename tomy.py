2946cef932fa805ea2788fbd78673ff3d69915c7
Jose C. Massón <jose@gcoop.coop>
2015-08-02 20:57:42 -0300

    Now we use tabulate to draw tables

--- tomy.py
+++ tomy.py
@@ -8,7 +8,7 @@ import pymysql
 import argparse
 import configparser
 
-
+from tabulate import tabulate
 from termcolor import colored, cprint
 from prompt_toolkit.shortcuts import get_input
 from prompt_toolkit.history import History
@@ -35,7 +35,6 @@ class TOMy:
 
     def __init__(self, db):
         """Constructor"""
-        self.get_color_config()
         self.arguments()
         self.database = db
         self.connect()
@@ -86,111 +85,11 @@ class TOMy:
         print(self.connection)
 
 
-    def format_output(self, headers_tuple, result):
-        """
-        This method formats the query output
-        Code inspired in http://code.activestate.com/recipes/\
-                577202-render-tables-for-text-interface/
-        """
-
-        column_names = list()
-        rows = list(result)
-
-        for h in headers_tuple:
-            column_names.append(h[0])
-
-        headers = column_names
-        fieldlen = []
-
-        ncols = len(headers)
-        print(result)
-        print(rows)
-        for i in headers:
-            _max = 0
-            for j in rows:
-                if len(str(j[i])) > _max:
-                    _max = len(str(j[i]))
-            fieldlen.append(_max)
-
-        for i in range(len(headers)):
-            if len(str(headers[i])) > fieldlen[i]:
-                fieldlen[i] = len(str(headers[i]))
-
-        width = sum(fieldlen) + (ncols - 1) * 3 + 4
-
-        bar = "-" * (width - 2)
-        bar = "+" + bar + "+"
-        pipe = '|'
-
-        if(self.color_config_dict['borders'] in self.colors):
-            bar = colored(bar, self.color_config_dict['borders'])
-            pipe = colored(pipe, self.color_config_dict['borders'])
-
-        #if(self.color_config_dict['borders_bold'] == 'True'):
-        #    bar = colored(bar, 'bold')
-        #    pipe = colored(pipe, 'bold')
-
-        out = [bar]
-        header = ""
-        for i in range(len(headers)):
-            header += pipe + " %s" % (str(headers[i]))\
-                + " " * (fieldlen[i] - len(str(headers[i]))) + " "
-        header += pipe
-        out.append(header)
-        out.append(bar)
-
-        for i in rows:
-            line = ""
-            for j in i:
-                field_value = self.None2NULL(str(i[j]))
-
-                if(self.color_config_dict['result'] in self.colors):
-                    r = colored(field_value,
-                                      self.color_config_dict['result'])
-                else:
-                    r = field_value
-
-                if(self.color_config_dict['result_bold'] == 'True'):
-                    r = colored(r, 'bold')
-
-                if(self.is_number(field_value) is True):
-                    line += pipe + " " * (len(str(i[j])) - len(field_value)) +\
-                        " %s " % r
-                else:
-                    line += pipe + " %s" % r + " " * (len(str(i[j])) -
-                                                      len(field_value)) + " "
-            out.append(line + pipe)
-
-        out.append(bar)
-        print("\r\n".join(out))
-
-
-    def get_color_config(self):
-        # Custom settings if you don't have set up in .config file
-        self.color_config_dict = {'borders': 'red', 'borders_bold': 'True',
-                                  'result': None, 'result_bold': 'False'}
-        self.colors = ('red', 'cyan', 'green', 'magenta', 'blue')
-        self.color_config = configparser.ConfigParser()
-        self.color_config.read('.config')
-
-        _color = self.color_config.get('colors', "borders",
-                                       vars=self.color_config_dict)
-        _borders_bold = self.color_config.get('colors', "borders_bold",
-                                              vars=self.color_config_dict)
-        _result = self.color_config.get('colors', "result",
-                                        vars=self.color_config_dict)
-        _result_bold = self.color_config.get('colors', "result_bold",
-                                             vars=self.color_config_dict)
-        self.color_config_dict['borders'] = _color
-        self.color_config_dict['borders_bold'] = _borders_bold
-        self.color_config_dict['result'] = _result
-        self.color_config_dict['result_bold'] = _result_bold
-
     def None2NULL(self, none):
         """
         I should learn how to use MySQLdb convertions
         """
-        if(none == 'None'):
+        if(none == None):
             return 'NULL'
         else:
             return none
@@ -216,12 +115,21 @@ class TOMy:
 
             with self.connection.cursor() as cursor:
                 messages = cursor.execute(text)
-                result = cursor.fetchall()
-                header = cursor.description
-                #print(result)
-                print(header)
-                if(header is not None):
-                    self.format_output(header, result)
+                fetchall = cursor.fetchall()
+                result = []
+                description = cursor.description
+                headers = []
+                rowcount = cursor.rowcount
+
+                for i in description:
+                    headers.append(i[0])
+
+                for record in fetchall:
+                    result.append([self.None2NULL(record[d]) for d in headers])
+
+                print(tabulate(result, headers, tablefmt="grid"))
+                print(str(rowcount) + ' rows\n')
+
 
         print('GoodBye!')
 
